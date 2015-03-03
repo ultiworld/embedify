f632dbf11204a39f05561f9e0c5880590de55d76
oburt <orionjburt@gmail.com>
2015-03-03 23:32:05 +0000

    v1.21 disable twitter parent tweets by default

--- embedify.js
+++ embedify.js
@@ -1,5 +1,5 @@
 /**
- * Embedify v1.20
+ * Embedify v1.21
  */
 window.Embedify = (function(window, document, $, undefined)
 {
@@ -279,7 +279,7 @@ window.Embedify = (function(window, document, $, undefined)
         'twitter-tweet',
         {
             regex: /(?:http:|https:|)(?:\/\/|)(?:www\.|)(?:twitter\.com)\/([\w_]+)\/([\w_]+)\/(\d+).*/gi,
-            html: '<blockquote class="embedify-embed twitter-tweet" align="center" lang="en">' +
+            html: '<blockquote class="embedify-embed twitter-tweet" align="center" lang="en" data-conversation="none">' +
                     '\t<a href="https://twitter.com/$1/$2/$3">https://twitter.com/$1/$2/$3</a>\n' +
                     '</blockquote>\n'
         }
